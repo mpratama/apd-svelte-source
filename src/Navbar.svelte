c1a7bbd5302226f18ae3fff4c451d8ed5b62908b
Arif Mulpratama <arifmul@gmail.com>
2021-07-01 17:40:59 +0700

    Berhasil ujicoba axios dan pemisahan komponen

---
+++ src/Navbar.svelte
@@ -0,0 +1,5 @@
+<nav class="nav">
+	<div class="container">
+		<a class="pagename current">APD v.3.1</a>
+	</div>
+</nav>
---
+++ src/TabelData.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let data;
+	
+</script>
+
+{#if !data}
+	<p>loading data...</p>
+{:else}
+	{#each data.barang as barang}
+		<p>{barang.nama}, {barang.jml}</p>
+	{/each}
+{/if}
